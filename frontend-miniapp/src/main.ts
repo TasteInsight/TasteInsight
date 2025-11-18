5683b0d4af7b7368b9b4e0f9f585f6c2dfc868c4
dongh <alicebob578@gmail.com>
2025-11-18 20:06:09 +0800

    fix(miniapp): 修复项目导入微信开发者助手后样式不渲染的问题

--- frontend-miniapp/src/main.ts
+++ frontend-miniapp/src/main.ts
@@ -1,7 +1,6 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import pinia from "./store";
-import './styles/tailwind.css'
 import './static/tailwind.css'
 export function createApp() {
   const app = createSSRApp(App);
