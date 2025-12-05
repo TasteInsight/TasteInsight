90dc4dc4e5f31b7b35e5fd2c836de63a1c93daa1
dongh <alicebob578@gmail.com>
2025-12-05 16:50:37 +0800

    feat(miniapp): 添加骨架屏组件 SkeletonBase 及其统一导出

---
+++ frontend-miniapp/src/components/skeleton/index.ts
@@ -0,0 +1,8 @@
+// 骨架屏组件统一导出
+export { default as SkeletonBase } from './SkeletonBase.vue';
+export { default as IndexSkeleton } from './IndexSkeleton.vue';
+export { default as CanteenSkeleton } from './CanteenSkeleton.vue';
+export { default as WindowSkeleton } from './WindowSkeleton.vue';
+export { default as DishDetailSkeleton } from './DishDetailSkeleton.vue';
+export { default as NewsListSkeleton } from './NewsListSkeleton.vue';
+export { default as NewsDetailSkeleton } from './NewsDetailSkeleton.vue';
